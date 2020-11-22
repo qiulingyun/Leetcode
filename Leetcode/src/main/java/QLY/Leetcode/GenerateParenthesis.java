1d2d7a48c1472f700a4876038eb24556b65d7fcb
qiulingyun <cloudinautumn@126.com>
2020-11-22 23:36:13 +0800

    tree and heap, Nov. 22

--- Leetcode/src/main/java/QLY/Leetcode/GenerateParenthesis.java
+++ Leetcode/src/main/java/QLY/Leetcode/GenerateParenthesis.java
@@ -15,22 +15,29 @@ public class GenerateParenthesis {
 
     private void backtrace(List<String> result, String path, int left, int right, int unpairedLeft){
         if (left == 0 && right == 0){
-
             result.add(path);
             return;
         }
 
-        for (int i = 0; i < 2; i++){
-            if (i == 0){
-                if (left == 0)
-                    continue;
-                backtrace(result, path + "(", left - 1, right, unpairedLeft + 1);
-            }else {
-                if (unpairedLeft == 0 || right == 0)
-                    continue;
-                backtrace(result, path + ")", left, right - 1, unpairedLeft - 1);
-            }
+        if (left != 0){
+            backtrace(result, path + "(", left - 1, right, unpairedLeft + 1);
         }
+
+        if (unpairedLeft > 0 && right > 0){
+            backtrace(result, path + ")", left, right - 1, unpairedLeft - 1);
+        }
+
+//        for (int i = 0; i < 2; i++){
+//            if (i == 0){
+//                if (left == 0)
+//                    continue;
+//                backtrace(result, path + "(", left - 1, right, unpairedLeft + 1);
+//            }else {
+//                if (unpairedLeft == 0 || right == 0)
+//                    continue;
+//                backtrace(result, path + ")", left, right - 1, unpairedLeft - 1);
+//            }
+//        }
     }
 
     public static void main(String[] args) {
---
+++ Leetcode/src/main/java/QLY/Leetcode/MedianFinder.java
@@ -0,0 +1,34 @@
+package QLY.Leetcode;
+
+import java.util.PriorityQueue;
+
+public class MedianFinder {
+    private PriorityQueue<Integer> small;
+    private PriorityQueue<Integer> big;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        small = new PriorityQueue<>((o1,o2)->o2-o1);
+        big = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        if (small.size() <= big.size()){
+            big.add(num);
+            small.add(big.poll());
+        }else if (small.size() > big.size()){
+            small.add(num);
+            big.add(small.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (small.size() > big.size()){
+            return small.peek();
+        }else if (small.size() < big.size()){
+            return big.peek();
+        }
+
+        return (small.peek()+big.peek())/2.0;
+    }
+}
