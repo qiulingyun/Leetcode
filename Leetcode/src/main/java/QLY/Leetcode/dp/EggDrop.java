1be110a13e80c3ce604da3177b50b78f6d9d9095
qiulingyun <cloudinautumn@126.com>
2020-12-21 19:07:25 +0800

    dp training  Dec.21

--- Leetcode/src/main/java/QLY/Leetcode/dp/EggDrop.java
+++ Leetcode/src/main/java/QLY/Leetcode/dp/EggDrop.java
@@ -1,38 +1,102 @@
 package QLY.Leetcode.dp;
 
+import java.util.Arrays;
+
+/**
+ * 887. 鸡蛋掉落
+ * 难度 困难
+ * 你将获得 K 个鸡蛋，并可以使用一栋从 1 到 N  共有 N 层楼的建筑。
+ * 每个蛋的功能都是一样的，如果一个蛋碎了，你就不能再把它掉下去。
+ * 你知道存在楼层 F ，满足 0 <= F <= N 任何从高于 F 的楼层落下的鸡蛋都会碎，从 F 楼层或比它低的楼层落下的鸡蛋都不会破。
+ * 每次移动，你可以取一个鸡蛋（如果你有完整的鸡蛋）并把它从任一楼层 X 扔下（满足 1 <= X <= N）。
+ * 你的目标是确切地知道 F 的值是多少。
+ * 无论 F 的初始值如何，你确定 F 的值的最小移动次数是多少？
+ *
+ *
+ * 示例 1：
+ * 输入：K = 1, N = 2
+ * 输出：2
+ * 解释：
+ * 鸡蛋从 1 楼掉落。如果它碎了，我们肯定知道 F = 0 。
+ * 否则，鸡蛋从 2 楼掉落。如果它碎了，我们肯定知道 F = 1 。
+ * 如果它没碎，那么我们肯定知道 F = 2 。
+ * 因此，在最坏的情况下我们需要移动 2 次以确定 F 是多少。
+ * 示例 2：
+ * 输入：K = 2, N = 6
+ * 输出：3
+ * 示例 3：
+ * 输入：K = 3, N = 14
+ * 输出：4
+ *
+ * 提示：
+ * 1 <= K <= 100
+ * 1 <= N <= 10000
+ */
 public class EggDrop {
 
 	// f[k][n] = f[k][n-1] + f[k-1][n-1] + 1
-	public int superEggDrop(int K, int N) {
+	public int superEggDrop2(int K, int N) {
 		if (K == 0) {
 			return 0;
 		}else if (K == 1) {
 			return N;
 		}
-		int[][] f = new int[K+1][N+1];	// K个鸡蛋，N层楼，最坏情况至少投掷？次，
+		int[][] dp = new int[K+1][N+1];	// K个鸡蛋，N层楼，最坏情况至少投掷？次，
         for (int i = 0; i < N+1; i++) {
-			f[0][i] = 0;
-			f[1][i] = i;
+			dp[0][i] = 0;
+			dp[1][i] = i;
 			
 		}
         for(int i = 0; i < K+1; i++){
-        	f[i][0] = 0;
+        	dp[i][0] = 0;
         }
-        for (int i = 2; i < f.length; i++) {
-			for (int j = 1; j < f[i].length; j++) {
-				f[i][j] = f[i][j-1] + f[i-1][j-1] + 1;
-				if(i == K && f[i][j] >= N){
+        for (int i = 2; i < dp.length; i++) {
+			for (int j = 1; j < dp[i].length; j++) {
+				dp[i][j] = dp[i][j-1] + dp[i-1][j-1] + 1;
+				if(i == K && dp[i][j] >= N){
 					return j;
 				}
 			}
 		}
         return -1;
     }
+
+	public int superEggDrop(int K, int N) {
+		// k个鸡蛋，n层楼，最少扔几次
+		int[][] dp = new int[K+1][N+1];
+		for (int i = 2; i <= K; i++){
+			Arrays.fill(dp[i], Integer.MAX_VALUE);
+		}
+
+		//1个鸡蛋
+		for (int i = 1; i <= N; i++){
+			dp[1][i] = i;
+		}
+		// 0层楼
+		for (int i = 1; i <= K; i++){
+			dp[i][0] = 0;
+		}
+		// 1层楼
+		for (int i = 1; i <= K; i++){
+			dp[i][1] = 1;
+		}
+
+		for (int i = 2; i <= K; i++){
+			for (int j = 2; j <= N; j++){
+				for (int k = 1; k <= j; k++){
+					dp[i][j] = Math.min(dp[i][j], Math.max(dp[i-1][k-1], dp[i][j-k]) + 1);
+				}
+
+			}
+		}
+
+		return dp[K][N];
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		EggDrop obj = new EggDrop();
-		System.out.println(obj.superEggDrop(1, 2));
+		System.out.println(obj.superEggDrop(3, 14));
 	}
 
 }
---
+++ Leetcode/src/main/java/QLY/Leetcode/dp/RegularExpressionMatching.java
@@ -0,0 +1,73 @@
+package QLY.Leetcode.dp;
+
+/**
+ * 10. 正则表达式匹配
+ * 难度 困难
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ *
+ * 示例 1：
+ * 输入：s = "aa" p = "a"
+ * 输出：false
+ * 解释："a" 无法匹配 "aa" 整个字符串。
+ * 示例 2:
+ * 输入：s = "aa" p = "a*"
+ * 输出：true
+ * 解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+ * 示例 3：
+ * 输入：s = "ab" p = ".*"
+ * 输出：true
+ * 解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+ * 示例 4：
+ * 输入：s = "aab" p = "c*a*b"
+ * 输出：true
+ * 解释：因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+ * 示例 5：
+ * 输入：s = "mississippi" p = "mis*is*p*."
+ * 输出：false
+ *
+ * 提示：
+ * 0 <= s.length <= 20
+ * 0 <= p.length <= 30
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+ * 保证每次出现字符 * 时，前面都匹配到有效的字符
+ */
+
+/**
+ * 参考： https://www.cnblogs.com/mfrank/p/10472663.html
+ */
+public class RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[s.length()][p.length()] = true;
+
+        for (int i = s.length(); i >= 0; i--) {
+            for (int j = p.length() - 1; j >= 0; j--) {
+                boolean curMatch = (i < s.length() &&
+                        (p.charAt(j) == s.charAt(i) ||
+                                p.charAt(j) == '.'));
+                if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+                    if (curMatch){
+                        dp[i][j] = dp[i][j + 2] || dp[i + 1][j];
+                    }else {
+                        dp[i][j] = dp[i][j + 2];
+                    }
+                } else {
+                    if (curMatch){
+                        dp[i][j] = dp[i + 1][j + 1];
+                    }
+                }
+            }
+        }
+        return dp[0][0];
+
+    }
+
+    public static void main(String[] args) {
+        RegularExpressionMatching regularExpressionMatching = new RegularExpressionMatching();
+        System.out.println(regularExpressionMatching.isMatch("aaa", "a*a"));
+    }
+}
